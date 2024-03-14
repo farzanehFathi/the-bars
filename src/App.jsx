12e5b76c72b17e13d578dfa6e4650b0a01140760
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-03-14 12:37:04 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div class="container m-auto">
+      <header>
+        <h1 class="text-3xl font-bold">This is my bars app</h1>
+      </header>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+export default function Card() {
+  return (
+    <div class="pomo-card">
+      <h2 class="pomo">Pomo</h2>
+      <p class="bar">.</p>
+      <h3 class="percent">55%</h3>
+    </div>
+  );
+}
+
+//  <div class="pomo">
+//         <h2>Pomo</h2>
+//       </div>
+//       <div class="bar">
+//         <p>progress bar</p>
+//       </div>
+//       <div class="percent">
+//         <h3>55%</h3>
+//       </div>
