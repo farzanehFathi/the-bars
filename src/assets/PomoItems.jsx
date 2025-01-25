0156fe31b54e4e163a3ec615e9ea88e942d89014
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2025-01-25 08:50:46 +0330

    database to static file
    db.json to PomoItems.jsx

---
+++ src/assets/PomoItems.jsx
@@ -0,0 +1,43 @@
+const PomoItems = () => {
+  const pomos = [
+    {
+      id: "1",
+      pomo: "🍅",
+      color: "#F03A17",
+      goal: 50,
+      clickIcon: "/icons/tomato-click-counter.png",
+    },
+    {
+      id: "2",
+      pomo: "🍋",
+      color: "#FCC219",
+      goal: 20,
+      clickIcon: "/icons/lemon-click-counter.png",
+    },
+    {
+      id: "3",
+      pomo: "🥦",
+      color: "#71A921",
+      goal: 40,
+      clickIcon: "/icons/broccoli-click-counter.png",
+    },
+    {
+      id: "4",
+      pomo: "🍉",
+      color: "#19B294",
+      goal: 50,
+      clickIcon: "/icons/watermelon-click-counter.png",
+    },
+    {
+      id: "5",
+      pomo: "🍇",
+      color: "#5F50A3",
+      goal: 50,
+      clickIcon: "/icons/grapes-click-counter.png",
+    },
+  ];
+
+  return pomos;
+};
+
+export default PomoItems;
--- src/context/PomoContext.jsx
+++ src/context/PomoContext.jsx
@@ -6,16 +6,18 @@ import {
 } from "solid-js";
 import { createStore } from "solid-js/store";
 
-const fetchPomos = async () => {
-  const res = await fetch("http://localhost:4000/pomos");
-  return res.json();
-};
+import PomoItems from "../assets/PomoItems";
+
+// const fetchPomos = async () => {
+//   const res = await fetch("http://localhost:4000/pomos");
+//   return res.json();
+// };
 
 export const PomoContext = createContext();
 
 export default function PomoContextProvider(props) {
   const [pomos, setPomos] = createStore([]);
-  const [allPomos] = createResource(fetchPomos);
+  const [allPomos] = createResource(PomoItems);
 
   createEffect(() => {
     if (allPomos()) {
