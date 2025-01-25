6aa9b225ff465858fa44a217e2c43b3594f31a00
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2025-01-25 09:07:02 +0330

    Moved pomoItems to PomoContext
    to solve deployment problem

--- src/context/PomoContext.jsx
+++ src/context/PomoContext.jsx
@@ -6,13 +6,49 @@ import {
 } from "solid-js";
 import { createStore } from "solid-js/store";
 
-import PomoItems from "../assets/pomoitems";
+const pomoItems = [
+  {
+    id: "1",
+    pomo: "🍅",
+    color: "#F03A17",
+    goal: 50,
+    clickIcon: "/icons/tomato-click-counter.png",
+  },
+  {
+    id: "2",
+    pomo: "🍋",
+    color: "#FCC219",
+    goal: 20,
+    clickIcon: "/icons/lemon-click-counter.png",
+  },
+  {
+    id: "3",
+    pomo: "🥦",
+    color: "#71A921",
+    goal: 40,
+    clickIcon: "/icons/broccoli-click-counter.png",
+  },
+  {
+    id: "4",
+    pomo: "🍉",
+    color: "#19B294",
+    goal: 50,
+    clickIcon: "/icons/watermelon-click-counter.png",
+  },
+  {
+    id: "5",
+    pomo: "🍇",
+    color: "#5F50A3",
+    goal: 50,
+    clickIcon: "/icons/grapes-click-counter.png",
+  },
+];
 
 export const PomoContext = createContext();
 
 export default function PomoContextProvider(props) {
   const [pomos, setPomos] = createStore([]);
-  const [allPomos] = createResource(PomoItems);
+  const [allPomos] = createResource(pomoItems);
 
   createEffect(() => {
     if (allPomos()) {
